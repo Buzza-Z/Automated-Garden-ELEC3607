0ed8c04f4cd36156cdf5e62b4f8e4fbf1237e047
Buzza-Z <42904216+Buzza-Z@users.noreply.github.com>
2019-06-07 23:05:40 +1000

    Add files via upload

---
+++ wtrevent.h
@@ -0,0 +1,46 @@
+/**
+    Copyright © 2019 Sam Shahrestani, Zachary Buzza
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the “Software”), to deal in the
+    Software without restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef WTREVENT_H
+#define WTREVENT_H
+
+#define WTREVENT_STACKSZ 4096
+#define WTREVENT_PRIORITY 5
+#define WTREVENT_POLLTIME (30 / portTICK_PERIOD_MS)
+
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+#include <stdint.h>
+
+class MotorBoard;
+class PState;
+
+typedef struct {
+    int chan;   
+} wtrEventParams;
+
+void newWtrEvent(int chan);
+int64_t waterTime();
+
+#endif
