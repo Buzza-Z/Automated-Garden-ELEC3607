c13bfdb025a076ce2b7ab252b45c018d5241c0d9
Buzza-Z <42904216+Buzza-Z@users.noreply.github.com>
2019-06-07 23:22:10 +1000

    Add files via upload

---
+++ ag_esp32/data/pstate.h
@@ -0,0 +1,72 @@
+/**
+    Copyright © 2019 Sam Shahrestani, Zachary Buzza
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the “Software”), to deal in the
+    Software without restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef PSTATE_H
+#define PSTATE_H
+#include <Arduino.h>
+#include <Preferences.h>
+#include <nvs_flash.h>
+
+#define PSTATE_NAMESPACE ("autog")
+
+#define PSTATE_VALID (0xAB)
+
+#define PSTATE_WIFI_MAX_LEN (128)
+
+/*
+ * A class that abstracts and simplifies the Preferences library,
+ * allowing for trivial storage and retreval of WiFi settings and
+ * channel settings.
+ */
+class PState
+{
+    private:
+        Preferences prefs;
+        bool _setup; // should the user setup the PState?
+
+    public:
+        PState();
+        // Initialises the NVS peripheral and checks for corruption.
+        void attachToNVS();
+
+        // Changes the stored WiFi SSID and password.
+        void setup(String wifi_ssid, String wifi_pwd);
+
+        // Checks if the stored WiFi details are invalid.
+        bool requiresSetup();
+
+        // Puts the stored SSID into a buffer.
+        size_t getWifiSSID(char* buff);
+
+        // Puts the stored WiFi password into a buffer.
+        size_t getWifiPwd(char* buff);
+
+        enum CHMode { Manual = 0, Timed = 1, Smart = 2, Genius = 3 };
+
+        // Changes the stored settings for a watering channel.
+        void setChSett (int chan, CHMode mode, double freq, double goal);
+
+        // Gets parts of the settings of a watering channel.
+        CHMode getChSettMode (int chan);
+        double getChSettFreq (int chan);
+        double getChSettGoal (int chan);
+};
+
+#endif
---
+++ ag_esp32/data/wmc.cpp
@@ -0,0 +1,171 @@
+/**
+    Copyright © 2019 Sam Shahrestani, Zachary Buzza
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the “Software”), to deal in the
+    Software without restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_seesaw.h"
+
+#include "wmc.h"
+
+static volatile uint64_t MB_wtr_delivered = 0;
+// IRAM_ATTR forces code into internal memory
+static void IRAM_ATTR MB_incWtr()
+{
+    MB_wtr_delivered++;
+}
+
+static volatile bool MB_sensor_outdated = true;
+static void IRAM_ATTR MB_void_sensor_data()
+{
+    MB_sensor_outdated = true;
+}
+
+
+MotorBoard::MotorBoard()
+{
+    _ss = new Adafruit_seesaw();
+    
+    pinMode(WMC_MCTL_FAULT, INPUT_PULLUP); // open drain on driver
+    pinMode(WMC_MCTL_EN, OUTPUT); // active low
+    digitalWrite(WMC_MCTL_EN, LOW);
+
+    // motor channels
+    pinMode(WMC_MCTL1, OUTPUT);
+    pinMode(WMC_MCTL2, OUTPUT);
+    pinMode(WMC_MCTL3, OUTPUT);
+    pinMode(WMC_MCTL4, OUTPUT);
+    _motorStates[0] = false;
+    _motorStates[1] = false;
+    _motorStates[2] = false;
+    _motorStates[3] = false;
+    
+    // water flow sensor
+    pinMode(WMC_WTR_FLOW, INPUT_PULLUP); 
+    attachInterrupt(digitalPinToInterrupt(WMC_WTR_FLOW), MB_incWtr, RISING);
+}
+
+void MotorBoard::setupPeripherals()
+{
+    // sensor timer
+    double apb_freq = ((READ_PERI_REG(RTC_APB_FREQ_REG)) & UINT16_MAX) << 12; // timer clk
+    
+    hw_timer_t* timer = timerBegin(1, apb_freq/1000000, true); // ~"TC_Configure", prescale for 1us ticks
+    timerAttachInterrupt(timer, MB_void_sensor_data, true); // ~"NVIC_EnableIRQ" + "TCX_Handler"
+    timerAlarmWrite(timer, 200000, true); // ~"TC_SetRC", 200000 * 1us ticks = 200ms
+    timerAlarmEnable(timer);
+    
+    // STEMMA header
+    Wire.begin(WMC_I2C_SDA, WMC_I2C_SCL, WMC_I2C_CLK);
+    
+    _ssState = _ss->begin(0x36);
+
+}
+
+void MotorBoard::_updateSensors()
+{
+    if (MB_sensor_outdated)
+    {
+        _lastSSTemp = _ss->getTemp();
+        _lastSSMoisture = _ss->touchRead(0);
+        MB_sensor_outdated = false;
+    }
+}
+
+bool MotorBoard::isSSconnected()
+{
+    return _ssState;
+}
+
+uint16_t MotorBoard::getSSMoisture()
+{
+	if (!_ssState)
+		return 0;
+
+    _updateSensors();
+      
+	return _lastSSMoisture;	
+}
+
+float MotorBoard::getSSTemp()
+{
+	if (!_ssState)
+		return 0;
+
+    _updateSensors();
+       	
+	return _lastSSTemp; // in celcius
+}
+
+int MotorBoard::setMotor(int motor, bool enable)
+{    
+    if (motor < 1 || motor > 4)
+        return -1;
+        
+    if (isFaultState())
+        return WMC_MOTOR_FAULT;
+    switch (motor)
+    {
+        case 1:
+            digitalWrite(WMC_MCTL1, enable);
+            break;
+        case 2:
+            digitalWrite(WMC_MCTL2, enable);
+            break;
+        case 3:
+            digitalWrite(WMC_MCTL3, enable);
+            break;
+        case 4:
+            digitalWrite(WMC_MCTL4, enable);
+            break;
+        default:
+            return 0;
+    }
+    _motorStates[motor-1] = enable;
+    
+    return enable;
+}
+
+int MotorBoard::getMotorState(int motor)
+{
+    if (motor < 1 || motor > 4)
+        return -1;
+        
+    if (isFaultState())
+        return WMC_MOTOR_FAULT;
+    
+    return _motorStates[motor-1];
+}
+
+bool MotorBoard::isFaultState()
+{
+    return digitalRead(WMC_MCTL_FAULT) == LOW;
+}
+
+double MotorBoard::getWaterDelivered()
+{
+    double wtr = MB_wtr_delivered / 450.0;
+    return wtr;
+}
+
+
+MotorBoard::~MotorBoard()
+{
+	delete _ss;
+}
